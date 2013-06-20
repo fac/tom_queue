98cb92e92a8658eeedc45812f86d2d4b6201d87c
Thomas Haggett <thomas@haggett.org>
2013-06-20 13:58:33 +0100

    Impose a time limit on all tests to avoid random blocking

--- spec/tom_queue/helper.rb
+++ spec/tom_queue/helper.rb
@@ -1,9 +1,14 @@
 require 'helper'
 require 'bunny'
 
+TomQueue.bunny = Bunny.new(:host => "localhost")
+TomQueue.bunny.start
+
 RSpec.configure do |r|
-  r.before(:all) do
-    TomQueue.bunny = Bunny.new(:host => "localhost")
-    TomQueue.bunny.start
+
+  # All tests should take < 2 seconds !!
+  r.around do |test|
+    Timeout.timeout(2) { test.call }
   end
+
 end
