ac52052fd9929a37fba68a9274c0375204e823b2
Thomas Haggett <thomas@haggett.org>
2013-06-28 16:47:04 +0100

    Add a deferred work set to handle the blocking on work

--- lib/tom_queue/deferred_work_set.rb
+++ lib/tom_queue/deferred_work_set.rb
@@ -1,6 +1,5 @@
 module TomQueue
 
-
   # Internal: This class wraps the pool of work who's run_at time has not been reached.
   # 
   # It also incorporates the logic and coordination required to stall a thread until the 
@@ -8,7 +7,82 @@ module TomQueue
   #
   class DeferredWorkSet
 
+    # Internal: A wrapper object to store the run at and the opaque
+    # work object inside the @work array.
+    class Element < Struct.new(:run_at, :work)
+      def <=> (other)
+        run_at <=> other.run_at
+      end
+      def sleep_interval
+        run_at - Time.now
+      end
+    end
+
+    def initialize
+      @mutex = Mutex.new
+      @condvar = ConditionVariable.new
+      @work = Set.new
+    end
+
+    # Public: Returns the integer number of elements in the set
+    #
+    # Returns integer
+    def size
+      @work.size
+    end
+
+    # Public: Block the calling thread until some work is ready to run
+    # or the timeout expires
+    #
+    # timeout - (Fixnum, seconds) how long to wait before timing out
+    #
+    def sleep(timeout)
+      timeout_end = Time.now + timeout
+      
+      @interrupt = false
+
+      @mutex.synchronize do
+        begin
+          end_time = [earliest_element.try(:run_at), timeout_end].compact.min
+          @condvar.wait(@mutex, end_time - Time.now)
+        end while Time.now < end_time and @interrupt == false
+      end
+
+    end
     
+    # Public: Interrupt anything sleeping on this set
+    def interrupt
+      @mutex.synchronize do
+        @interrupt = true
+        @condvar.signal
+      end
+    end
+
+    # Public: Add some work to the set
+    #
+    # run_at - (Time) when the work is to be run
+    # work   - the DeferredWork object
+    #
+    def schedule(run_at, work)
+      @mutex.synchronize do
+        @work << Element.new(run_at, work)
+        @condvar.signal
+      end
+    end
+
+    # Public: Returns the temporally "soonest" element in the set
+    # i.e. the work that is next to be run
+    #
+    # Returns a DeferredWork instance or
+    #         nil if there is no work in the set
+    def earliest
+      earliest_element.try(:work)
+    end
+
+    # Internal: The earliest element (i.e. wrapper object)
+    def earliest_element
+      @work.sort.first
+    end
   end
 
 end
--- spec/tom_queue/deferred_work_set_spec.rb
+++ spec/tom_queue/deferred_work_set_spec.rb
@@ -1,11 +1,79 @@
 require 'tom_queue/helper'
 
 describe TomQueue::DeferredWorkSet do
+  let(:set) { TomQueue::DeferredWorkSet.new }
 
   it "should be creatable" do
-    TomQueue::DeferredWorkSet.new.should be_a(TomQueue::DeferredWorkSet)
+    set.should be_a(TomQueue::DeferredWorkSet)
   end
 
+  it "should allow work to be scheduled" do
+    set.schedule(Time.now + 0.2, "something")
+    set.schedule(Time.now + 0.3, "else")
+    set.size.should == 2
+  end
 
+  describe "earliest" do
+
+    it "should return nil if there is no work in the set" do
+      set.earliest.should be_nil
+    end
+
+    it "should return the only item if there is one item in the set" do
+      work = mock("Work")
+      set.schedule( Time.now + 0.3, work)
+      set.earliest.should == work
+    end
+
+    it "should return the item in the set with the lowest run_at value" do
+      set.schedule( Time.now + 0.2, work1 = mock("Work") )
+      set.schedule( Time.now + 0.1, work2 = mock("Work") )
+      set.schedule( Time.now + 0.3, work3 = mock("Work") )
+      set.earliest.should == work2
+    end
+
+  end
+
+  describe "sleep" do
+
+    it "should block for the timeout value if there is no work in the queue" do
+      start_time = Time.now
+      set.sleep(0.1)
+      Time.now.should > start_time + 0.1
+    end
+
+    it "should block until the earliest work in the set" do
+      start_time = Time.now
+      set.schedule(start_time + 1.5, "work")
+      set.schedule(start_time + 0.1, "work")
+      set.sleep(10)
+      Time.now.should > start_time + 0.1
+      Time.now.should < start_time + 0.2
+    end
+
+    it "should return immediately if it is interrupted by an external thread" do
+      Thread.new { sleep 0.1; set.interrupt }
+      start_time = Time.now
+      set.schedule(start_time + 1.5, "work")
+      set.schedule(start_time + 5, "work")
+      set.sleep(10)
+      Time.now.should > start_time + 0.1
+      Time.now.should < start_time + 0.2
+    end
+
+    it "should block until the earliest work, even if earlier work is added after the block" do
+      start_time = Time.now
+      Thread.new do
+        sleep 0.1
+        set.schedule(start_time + 0.2, "early")
+      end
+      set.schedule(start_time + 1.5, "late")
+      set.sleep(10)
+      Time.now.should > start_time + 0.2
+      Time.now.should < start_time + 0.3
+
+    end
+
+  end
 
 end
