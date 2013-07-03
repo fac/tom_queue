0a144865a4349d174055079f5947f58a840289f2
Thomas Haggett <thomas@haggett.org>
2013-07-04 00:10:35 +0100

    Removes old commented tests

--- spec/tom_queue/deferred_work_manager_spec.rb
+++ spec/tom_queue/deferred_work_manager_spec.rb
@@ -136,40 +136,4 @@ describe TomQueue::DeferredWorkManager do
     it "should not fail if the queue hasn't been created"
   end
 
-
-  # let(:queue_manager) { TomQueue::QueueManager.new('fa.test')}
-  # let(:manager) { TomQueue::DeferredWorkManager.new('fa.test', queue_manager)}
-
-  # before do
-  #   manager.purge!
-  # end
-
-  # describe "creation" do  
-  #   it "should be created with a prefix" do
-  #     manager.prefix.should == 'fa.test'
-  #   end
-  #   it "should be created with a delegate" do
-  #     manager.delegate.should == queue_manager
-  #   end
-  # end
-
-  # describe "handle_deferred(work, opts)" do
-
-  #   it "should raise an argument error if the :run_at option isn't specified" do
-  #     lambda {
-  #       manager.handle_deferred("work", {})
-  #     }.should raise_exception(ArgumentError, /:run_at must be specified/)
-  #   end
-  #   it "should raise an argument error if the :run_at isn't a ruby time" do
-  #     lambda {
-  #       manager.handle_deferred("work", {:run_at => "in about half an hour"})
-  #     }.should raise_exception(ArgumentError, /:run_at must be a Time object/)
-  #   end
-  #   it "should raise an argument error if the work isn't a string" do
-  #     lambda {
-  #       manager.handle_deferred({"foo" => :bar}, {:run_at => Time.now})
-  #     }.should raise_exception(ArgumentError, /work must be a string/)
-  #   end
-  # end
-
 end
