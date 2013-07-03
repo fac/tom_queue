381368b6fb35f005c3bb36874b8c65a31a0aa0c5
Thomas Haggett <thomas@haggett.org>
2013-07-04 00:08:31 +0100

    Tweak crappy comment

--- lib/tom_queue/deferred_work_set.rb
+++ lib/tom_queue/deferred_work_set.rb
@@ -1,6 +1,7 @@
 module TomQueue
 
-  # Internal: This class wraps the pool of work who's run_at time has not been reached.
+  # Internal: This class wraps the pool of work items that are waiting for their run_at 
+  # time to be reached.
   # 
   # It also incorporates the logic and coordination required to stall a thread until the 
   # work is ready to run.
