df758d2abd6884466304cf6c98d9c2056a43c386
Paul Smith <paul@freeagent.com>
2017-06-06 16:54:06 +0100

    Improve failed job spec

--- test_app/spec/tom_queue/job_failure_spec.rb
+++ test_app/spec/tom_queue/job_failure_spec.rb
@@ -19,21 +19,17 @@ describe "Job Failure", worker: true do
   it "should increment attempts" do
     job = Delayed::Job.enqueue(FailingJob.new)
     expect(job.attempts).to eq(0)
-    sleep(MINIMUM_JOB_DELAY)
+    sleep(1)
     expect(job.reload.attempts).to eq(1)
-  end
-
-  it "should set last_error" do
-    job = Delayed::Job.enqueue(FailingJob.new)
-    sleep(MINIMUM_JOB_DELAY)
-    expect(job.reload.last_error).to match(/Epic Fail!/)
+    expect(job.last_error).to match(/Epic Fail!/)
   end
 
   it "should set failed_at on permanent failure" do
     job = Delayed::Job.enqueue(FailingJob.new)
-    sleep(MINIMUM_JOB_DELAY)
+    sleep(1)
     expect(job.reload.attempts).to eq(1)
     expect(job.failed_at).to be_nil
+    sleep(1)
     expect(job.reload.attempts).to eq(2)
     expect(job.failed_at).to be_a(DateTime)
   end
