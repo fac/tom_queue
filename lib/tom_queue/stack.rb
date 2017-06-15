c64fb32b48c681ae591c07e920eabeb3c1d25134
Paul Smith <paul@freeagent.com>
2017-06-15 10:11:25 +0100

    "middleware" -> "layer"

--- lib/tom_queue/stack.rb
+++ lib/tom_queue/stack.rb
@@ -2,25 +2,25 @@ module TomQueue
   class Stack
     TERMINATOR = lambda { |work, options| [work, options] }
 
-    # Public: Insert a middleware layer at the beginning of the stack.
-    # middleware - a descendant of TomQueue::Stack::Layer
-    # options - a hash of options to use when instantiating the middleware
+    # Public: Insert a layer at the beginning of the stack.
+    # layer - a descendant of TomQueue::Stack::Layer
+    # options - a hash of options to use when instantiating the layer
     #
     # Returns nothing
-    def self.insert(middleware, options = {})
-      @stack = middleware.new(stack, options)
+    def self.insert(layer, options = {})
+      @stack = layer.new(stack, options)
     end
 
-    # Public: Append a middleware layer to the end of the stack.
-    # middleware - a descendant of TomQueue::Stack::Layer
-    # options - a hash of options to use when instantiating the middleware
+    # Public: Append a layer to the end of the stack.
+    # layer - a descendant of TomQueue::Stack::Layer
+    # options - a hash of options to use when instantiating the layer
     #
     # Returns nothing
-    def self.use(middleware, options = {})
+    def self.use(layer, options = {})
       if stack
-        stack.append(middleware, options)
+        stack.append(layer, options)
       else
-        @stack = middleware.new(nil, options)
+        @stack = layer.new(nil, options)
       end
     end
 
@@ -33,7 +33,7 @@ module TomQueue
       (stack || TERMINATOR).call(work, options)
     end
 
-    # Internal: The class' middleware stack
+    # Internal: The class' layer stack
     #
     # Returns a TomQueue::Stack::Layer instance or nil
     def self.stack
@@ -48,7 +48,7 @@ module TomQueue
         @config = config
       end
 
-      # Public: Execute the middleware layer.
+      # Public: Execute the stack
       # Subclasses should _usually_ chain.call(work, options) unless they want to
       # stop execution of the stack in which case they should just return
       #
@@ -60,17 +60,18 @@ module TomQueue
         return [work, options]
       end
 
-      # Public: Replaces the terminator with a new middleware, or passes it
-      # to the next layer in the chain
-      # middleware - a descendant of TomQueue::Stack::Layer
-      # options - a hash of options to use when instantiating the middleware
+      # Public: Adds a new layer if this layer is at the bottom of the stack,
+      # or passes it to the next layer in the chain
+      #
+      # layer - a descendant of TomQueue::Stack::Layer
+      # options - a hash of options to use when instantiating the layer
       #
       # Returns nothing
-      def append(middleware, options = {})
+      def append(layer, options = {})
         if @chain.nil?
-          @chain = middleware.new(nil, options)
+          @chain = layer.new(nil, options)
         else
-          @chain.append(middleware, options)
+          @chain.append(layer, options)
         end
       end
 
